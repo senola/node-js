89098d4cfa2b8ff523cf6cba8f84c8d9949bee4c
senola <xinshouzhilu@163.com>
2014-07-30 21:17:38 +0800

    my-first-io
    my-first-io

--- my-first-io/program.js
+++ my-first-io/program.js
@@ -1,7 +1,7 @@
 // my first io 
 // 加载 fs模块 
 var fs = require('fs'); 
-var filePath = process.argv[1]; //获取文件地址
+var filePath = process.argv[2]; // 输入的第一个参数
 var buf = fs.readFileSync(filePath);
 var bufString = buf.toString(); //获取buffer对象并转换成字符串
 //console.log(bufString);
